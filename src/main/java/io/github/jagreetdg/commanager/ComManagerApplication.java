44498bb4f4c86995a753c1d0317b241102ed05ff
Jagreet Das Gupta <jagreetdg@gmail.com>
2022-06-20 15:18:12 +0530

    Initial Commit

---
+++ src/main/java/io/github/jagreetdg/commanager/ComManagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.jagreetdg.commanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jagreetdg/commanager/ComManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jagreetdg.commanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
